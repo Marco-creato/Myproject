ce5c81ba618b4f7333def205807f90c5ed951486
Marco-creato <sacramentoaurelio@gmail.com>
2024-09-06 19:12:34 -0300

    Add files via upload
    444....

---
+++ #calculos matematicos basicos.py
@@ -0,0 +1,26 @@
+#calculos matematicos basicos
+X = int(input("Digite um numero: "))
+Y = int(input("Digite outro numero: "))
+
+SOMA = X + Y
+print("O resultado da soma é", SOMA)
+
+SUBTRAÇÃO = X - Y
+print("O resulado da subtração é", SUBTRAÇÃO)
+
+MULTIPLICAÇÃO = X * Y
+print("O resultado da multiplicação é", MULTIPLICAÇÃO)
+
+DIVISÃO = X / Y
+print(f"O resultado da divisão é {DIVISÃO}")
+
+#calculos complexos
+
+Z = int(input("Digite um terceiro numero: "))
+O = int(input("Digite um quarto numero: "))
+
+cal = X + Y * Z
+print(f"O resultado do calculo é {cal}")
+
+cal2 = X - Y * O / Z
+print(f"O resultado do calculo é {cal2}")
---
+++ #sentença matematica.py
@@ -0,0 +1,13 @@
+#sentença matematica
+X = 10
+Y = 20
+Z = 30
+
+VALOR = X * (Y / 10 + Z / 10) - 25
+print(f"O resultado da sentença é {VALOR}")
+
+VALOR = ((Y - X) + (Z - X)) / 3
+print(f"O resultado da sentença é {VALOR}")
+
+VALOR = (X +(Y * 2 +(Z / 10))) - 18
+print(f"O resultado da sentença é {VALOR}")
