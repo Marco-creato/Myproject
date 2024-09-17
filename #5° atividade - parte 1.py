3341903a469a0f360a9844e47c71253bae539fb5
Marco-creato <sacramentoaurelio@gmail.com>
2024-09-16 21:22:22 -0300

    Add files via upload

--- #5° atividade - parte 1.py
+++ #5° atividade - parte 1.py
@@ -5,31 +5,34 @@ GA = 5.75
 D = 4.90
 soma_litros = 0
 preço = 0
+resp = "S"
 
-
-for com in range(1):
+while resp == "S" or resp == "s":
     Combustivel = input("Qual dos seguintes tipos de combustivel você deseja(E/GC/GA/D)? ")
-    if Combustivel == "E":
+    if Combustivel == "E" or Combustivel == "e":
         litros = int(input("Quantos litros de Etanol você ira querer? "))
         soma_litros = soma_litros + litros
         preço = preço + soma_litros
         preço = preço * E
         print(f"O preço do Etanol é R${preço:.2f}")
         
-    elif Combustivel == "GC":
+    elif Combustivel == "GC" or Combustivel == "gc":
         litros = int(input("Quantos litros de Gasolina comum você ira querer? "))
         soma_litros = soma_litros + litros
         preço = preço + soma_litros * GC
         print(f"O preço do Gasolina comum é R${preço:.2f}")
         
-    elif Combustivel == "GA":
+    elif Combustivel == "GA" or Combustivel == "ga":
         litros = int(input("Quantos litros Gasolina Aditivada você ira querer? "))
         soma_litros = soma_litros + litros
         preço = preço + soma_litros * GA
         print(f"O preço do Gasolina Aditivada é R${preço:.2f}")
         
-    elif Combustivel == "D":
+    elif Combustivel == "D" or Combustivel == "d":
         litros = int(input("Quantos litros de Diesel você ira querer? "))
         soma_litros = soma_litros + litros
         preço = preço + soma_litros * D
         print(f"O preço do Diesel é R${preço:.2f}")
+    else:
+        print("Esse tipo de combustivel não existe!!")
+    resp = input("Deseja continuar (S/N)? ")
